1dfd70ecac8f16b9311d85c5364d2c6bbd40b6bd
Michael Frey <michael.frey@bergauer.ch>
2025-06-19 16:28:16 +0200

    asd

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -7,12 +7,17 @@ const API = '/api'
 function Map() {
   const [layers, setLayers] = useState([])
   const [selected, setSelected] = useState('')
+  const [useMapServer, setUseMapServer] = useState(false)
 
   useEffect(() => {
     fetch(`${API}/tms`).then(r => r.json()).then(setLayers).catch(console.error)
   }, [])
 
   const current = layers.find(l => l.id === selected)
+
+  const toggleMapServer = () => {
+    setUseMapServer(!useMapServer)
+  }
 
   return (
     <>
@@ -23,11 +28,18 @@ function Map() {
             <option key={l.id} value={l.id}>{l.id}</option>
           ))}
         </select>
+        <button onClick={toggleMapServer} className="ml-2 p-1 border">
+          {useMapServer ? 'Disable MapServer' : 'Enable MapServer'}
+        </button>
       </div>
       <MapContainer center={[47.3769, 8.5417]} zoom={13} style={{ height: '100%', width: '100%' }}>
         <TileLayer
-          url="https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-grau/default/current/3857/{z}/{x}/{y}.jpeg"
-          attribution="&copy; <a href='https://www.geo.admin.ch/'>geo.admin.ch</a>"
+          url={useMapServer
+            ? "http://10.254.64.14/styles/basic-preview/512/{z}/{x}/{y}.png"
+            : "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-grau/default/current/3857/{z}/{x}/{y}.jpeg"}
+          attribution={useMapServer
+            ? "&copy; <a href='https://www.maptiler.com/'>Maptiler</a>"
+            : "&copy; <a href='https://www.geo.admin.ch/'>geo.admin.ch</a>"}
         />
         {current && (
           <TileLayer url={`${current.url}/{z}/{x}/{y}.png`} />
